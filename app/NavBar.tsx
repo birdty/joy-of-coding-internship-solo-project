a96692e2073a89542ee20ccce276cba215d17ba0
Tyler Bird <birdty@epromo.com>
2024-01-01 12:12:45 -0700

    o - added radix ui

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,11 +1,17 @@
+"use client";
 import React from "react";
 import Link from "next/link";
 import { AiFillBug } from "react-icons/ai";
+import { usePathname } from "next/navigation";
+import classnames from "classnames";
+
 const NavBar = () => {
   const issues = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
   ];
+
+  const currentPath = usePathname();
 
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
@@ -15,9 +21,13 @@ const NavBar = () => {
       <ul className="flex space-x-6">
         {issues.map((issue) => (
           <Link
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
             key={issue.href}
             href={issue.href}
+            className={classnames({
+              "text-zinc-900": issue.href === currentPath,
+              "text-zinc-500": issue.href !== currentPath,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
           >
             {issue.label}
           </Link>
